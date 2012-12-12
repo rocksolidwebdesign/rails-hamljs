8cfc2a9713b9d360365319b11077d1f3a792065d
Vaughn Draughon <vaughn@rocksolidwebdesign.com>
2012-12-12 14:16:06 -0700

    using uglyog's implementation instead and including/requiring coffeescript

--- lib/rails-hamljs.rb
+++ lib/rails-hamljs.rb
@@ -1,6 +1,7 @@
 require 'rails-hamljs/version'
 require 'tilt'
 require 'sprockets'
+require 'open-uri'
 
 module Hamljs
   class Template < Tilt::Template
@@ -13,9 +14,10 @@ module Hamljs
       'u2028' => "\u2028",
       'u2029' => "\u2029"
     }
-    JS_ESCAPES = JS_UNESCAPES.invert
+
+    JS_ESCAPES          = JS_UNESCAPES.invert
     JS_UNESCAPE_PATTERN = /\\(#{Regexp.union(JS_UNESCAPES.keys)})/
-    JS_ESCAPE_PATTERN = Regexp.union(JS_ESCAPES.keys)
+    JS_ESCAPE_PATTERN   = Regexp.union(JS_ESCAPES.keys)
 
     def self.engine_initialized?
       true
@@ -27,13 +29,58 @@ module Hamljs
     def prepare
     end
 
+    def get_coffeescript_source
+      @@coffeescript_source ||= open('http://jashkenas.github.com/coffee-script/extras/coffee-script.js').read
+    end
+
+    def get_underscore_source
+      #@@underscore_source ||= open('http://underscorejs.org/underscore-min.js').read
+      @@underscore_source ||= File.read(File.expand_path("../../vendor/assets/javascripts/uglyog-haml-js/ext/underscore.js", __FILE__))
+    end
+
+    def get_underscore_string_source
+      #@@underscore_string_source ||= open('http://epeli.github.com/underscore.string/dist/underscore.string.min.js').read
+      @@underscore_string_source ||= File.read(File.expand_path("../../vendor/assets/javascripts/uglyog-haml-js/ext/underscore.string.js", __FILE__))
+    end
+
+    def get_hamljs_source
+      @@hamljs_source ||= get_uglyog_hamljs_source
+    end
+
+    def get_jsonjs_source
+      @@jsonjs_source ||= File.read(File.expand_path("../../vendor/assets/javascripts/uglyog-haml-js/ext/json2.js", __FILE__))
+    end
+
+    def get_creationix_hamljs_source
+      @@creationix_hamljs_source ||= File.read(File.expand_path("../../vendor/assets/javascripts/creationix-haml-js/haml.js", __FILE__))
+    end
+
+    def get_uglyog_hamljs_source
+      @@uglyog_hamljs_source ||= [
+        get_jsonjs_source,
+        get_underscore_source,
+        get_underscore_string_source,
+        File.read(File.expand_path("../../vendor/assets/javascripts/uglyog-haml-js/haml.min.js", __FILE__))
+        #open('https://raw.github.com/uglyog/clientside-haml-js/master/lib/haml.min.js').read
+      ].join(" ")
+
+      #@@uglyog_hamljs_source ||= File.read(File.expand_path("../../vendor/assets/javascripts/uglyog-haml-js/haml.min.js", __FILE__))
+    end
+
     def compile(source, options = {})
-      source = source.dup
+      s = source.dup
 
-      js_escape!(source)
+      cmd = "haml.compileHaml({source: \"#{js_escape!(s)}\", generator: 'coffeescript'}).toString()"
 
-      context = ExecJS.compile(File.read(File.expand_path("../../vendor/assets/javascripts/haml.js", __FILE__)))
-      context.eval("Haml('#{source}').toString()");
+      lib_sources = [
+        get_coffeescript_source,
+        get_hamljs_source
+      ]
+
+      context = ExecJS.compile(lib_sources.join(" "))
+      result = context.eval(cmd)
+
+      result
     end
 
     def evaluate(scope, locals, &block)
@@ -43,7 +90,16 @@ module Hamljs
 
   protected
     def js_escape!(source)
-      source.gsub!(JS_ESCAPE_PATTERN) { |match| '\\' + JS_ESCAPES[match] }
+      puts "JS_ESCAPE_PATTERN: #{}"
+      puts JS_ESCAPE_PATTERN.inspect
+      puts "JS_ESCAPES:"
+      puts JS_ESCAPES.inspect
+      source.gsub!(/\n/, '\n')
+      source.gsub!(/\r/, '\r')
+      source.gsub!(/\t/, '\t')
+      source.gsub!(/"/, '\"')
+      source.gsub!(/\u2028/, '\u2028')
+      source.gsub!(/\u2029/, '\u2029')
       source
     end
   end
